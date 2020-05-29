50e3441eaacdf9a28717336bece4c9b0d6cb20d4
Daniel Rojas <github@danielrojas.net>
2020-05-30 00:46:59 +0200

    Add command line argument parsing (input/output file)

---
+++ src/batch.py
@@ -0,0 +1,7 @@
+import yaml2wireviz
+
+# TODO: make examples (progressively more complex), batch process all of them
+yaml2wireviz.parse('../examples/example1.yml')
+yaml2wireviz.parse('../examples/example2.yml')
+yaml2wireviz.parse('../examples/ferrules.yml')
+yaml2wireviz.parse('../examples/bundles.yml')
