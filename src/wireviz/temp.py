ef9f36f1b0e3e85419035631ab0940e33b43e880
Daniel Rojas <github@danielrojas.net>
2020-10-24 01:45:01 +0200

    Component generation seems to work

---
+++ src/wireviz/temp.py
@@ -0,0 +1,36 @@
+        # check which section the first item belongs to
+        # first_item = get_first_item_in_entry(connection_set[0])
+        # print('first item:', first_item)
+        #
+        # if designators_and_templates[first_item] in template_connectors:
+        #     expected_index = 0
+        #     print('Set starts with con')
+        # elif designators_and_templates[first_item] in template_cables:
+        #     expected_index = 1
+        #     print('Set starts with cable')
+        # else:
+        #     raise Exception('What')
+
+        # check that all entries alternate between cables and connectors
+        # TODO
+        # for entry in connection_set:
+        #     for item in get_item_list(entry):
+        #         if
+
+
+
+
+
+        # for index, section in enumerate(alternating_sections):
+        #     if first_item in yaml_data[section]:
+        #         expected_index = index
+        #         break
+        # else:
+        #     raise Exception('First item not found anywhere.')
+        # expected_index = 1 - expected_index  # flip once since it is flipped back at the *beginning* of every loop
+        # for entry in connection_set:
+        #     pass
+
+        # generate items
+
+        # connect items
