733e21182d69a39d96996146fe812ced7700a31f
KV <kvid@users.noreply.github.com>
2021-02-23 20:56:44 +0100

    Avoid transparent cable nodes
    Bug: Node attribute fillcolor is ignored unless style contains filled.
    When cable nodes get an empty style (unless bundle), the node background
    color become equal to the graph background color as it was transparent.
    
    Fix: Make the style contain filled.

--- src/wireviz/Harness.py
+++ src/wireviz/Harness.py
@@ -95,6 +95,7 @@ class Harness:
                  bgcolor=wv_colors.translate_color(self.options.bgcolor, "HEX"),
                  nodesep='0.33',
                  fontname=self.options.fontname)
+        # Currently, fontname seems to be the only of these node attributes not overridden for each node.
         dot.attr('node', shape='record',
                  style='filled',
                  fillcolor='white',
@@ -330,7 +331,8 @@ class Harness:
 
             html = '\n'.join(html)
             dot.node(cable.name, label=f'<\n{html}\n>', shape='box',
-                     style='filled,dashed' if cable.category == 'bundle' else '', margin='0', fillcolor='white')
+                     style='filled,dashed' if cable.category == 'bundle' else 'filled',
+                     margin='0', fillcolor='white')
 
         return dot
 
