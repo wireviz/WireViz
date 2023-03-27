477a3f770605d740ba26199ec0d4776f408d36e9
Daniel Rojas <github@danielrojas.net>
2023-03-27 13:45:05 +0200

    Update interim version number

--- src/wireviz/__init__.py
+++ src/wireviz/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Please don't import anything in this file to avoid issues when it is imported in setup.py
 
-__version__ = "0.4-dev"
+__version__ = "0.4-dev-refactored"
 
 CMD_NAME = "wireviz"  # Lower case command and module name
 APP_NAME = "WireViz"  # Application name in texts meant to be human readable
